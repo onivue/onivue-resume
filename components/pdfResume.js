958c0e5f1c720c469489b3bdbac8cacb14a40268
omnia-web <omnia.webmail@gmail.com>
2021-11-23 21:19:16 +0100

    font fix

--- components/pdfResume.js
+++ components/pdfResume.js
@@ -7,18 +7,133 @@ import {
   Font,
   Image,
 } from '@react-pdf/renderer'
+Font.register({
+  family: 'Open Sans',
+  fonts: [
+    {
+      src: '/fonts/open-sans-regular.ttf',
+    },
+    {
+      src: '/fonts/open-sans-600.ttf',
+      fontWeight: 600,
+    },
+  ],
+})
 // Font.register({
-//   family: 'Open Sans',
-//   fonts: [
-//     {
-//       src: '/fonts/open-sans-regular.ttf',
-//     },
-//     {
-//       src: '/fonts/open-sans-600.ttf',
-//       fontWeight: 600,
-//     },
-//   ],
+//   family: 'Oswald',
+//   src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf',
 // })
+const styles = StyleSheet.create({
+  body: {
+    paddingTop: 35,
+    paddingBottom: 35,
+    fontFamily: 'Oswald',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    left: '0',
+    right: '0',
+    top: '0',
+    // height: '100%',
+  },
+  sectionSide: {
+    position: 'absolute',
+    textAlign: 'left',
+    zIndex: '10',
+    top: '0',
+    width: 220,
+    paddingTop: 40,
+    paddingHorizontal: 24,
+    backgroundColor: '#f7fafc',
+  },
+  sectionSideOverlay: {
+    position: 'absolute',
+    height: '297mm',
+    zIndex: '20',
+    top: '0',
+    width: 220,
+    backgroundColor: '#f7fafc',
+  },
+  sectionWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: 220,
+    paddingHorizontal: 30,
+    fontSize: 10,
+  },
+  section: {
+    // textAlign: 'center',
+    // height: '100%',
+  },
+  sectionH1: {
+    color: '#5b21b6',
+    fontWeight: '600',
+    fontSize: 18,
+    paddingBottom: '10px',
+  },
+  sectionSeparator: {
+    height: '3px',
+    width: '100%',
+    backgroundColor: '#f7fafc',
+    marginVertical: '15px',
+  },
+  sectionH2: {
+    fontSize: 12,
+    fontWeight: '600',
+    marginBottom: '4px',
+  },
+  sectionH3: {
+    fontSize: 10,
+    fontWeight: '600',
+    marginBottom: '8px',
+  },
+  name: {
+    fontSize: 30,
+    marginBottom: 12,
+  },
+  jobTitle: {
+    fontSize: 20,
+  },
+  cvSection: {
+    marginTop: 24,
+  },
+  cvSectionTitle: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  cvSectionText: {
+    fontSize: 14,
+  },
+  csvTagWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  csvTag: {
+    fontSize: 8,
+    borderRadius: '50%',
+    color: 'white',
+    backgroundColor: '#5b21b6',
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    marginRight: 4,
+    marginVertical: 2,
+  },
+  item: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  bulletPoint: {
+    width: 10,
+    fontSize: 10,
+    color: '#5b21b6',
+  },
+  itemContent: {
+    flex: 1,
+    fontSize: 10,
+  },
+})
 
 const ProgressBar = ({ language, progress }) => (
   <>
@@ -174,118 +289,3 @@ export const MyDoc = ({ text }) => {
     </Document>
   )
 }
-Font.register({
-  family: 'Oswald',
-  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf',
-})
-const styles = StyleSheet.create({
-  body: {
-    paddingTop: 35,
-    paddingBottom: 35,
-    fontFamily: 'Oswald',
-  },
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    left: '0',
-    right: '0',
-    top: '0',
-    // height: '100%',
-  },
-  sectionSide: {
-    position: 'absolute',
-    textAlign: 'left',
-    zIndex: '10',
-    top: '0',
-    width: 220,
-    paddingTop: 40,
-    paddingHorizontal: 24,
-    backgroundColor: '#f7fafc',
-  },
-  sectionSideOverlay: {
-    position: 'absolute',
-    height: '297mm',
-    zIndex: '20',
-    top: '0',
-    width: 220,
-    backgroundColor: '#f7fafc',
-  },
-  sectionWrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    marginLeft: 220,
-    paddingHorizontal: 30,
-    fontSize: 10,
-  },
-  section: {
-    // textAlign: 'center',
-    // height: '100%',
-  },
-  sectionH1: {
-    color: '#5b21b6',
-    fontWeight: '600',
-    fontSize: 18,
-    paddingBottom: '10px',
-  },
-  sectionSeparator: {
-    height: '3px',
-    width: '100%',
-    backgroundColor: '#f7fafc',
-    marginVertical: '15px',
-  },
-  sectionH2: {
-    fontSize: 12,
-    fontWeight: '600',
-    marginBottom: '4px',
-  },
-  sectionH3: {
-    fontSize: 10,
-    fontWeight: '600',
-    marginBottom: '8px',
-  },
-  name: {
-    fontSize: 30,
-    marginBottom: 12,
-  },
-  jobTitle: {
-    fontSize: 20,
-  },
-  cvSection: {
-    marginTop: 24,
-  },
-  cvSectionTitle: {
-    fontSize: 18,
-    marginBottom: 5,
-  },
-  cvSectionText: {
-    fontSize: 14,
-  },
-  csvTagWrapper: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-  },
-  csvTag: {
-    fontSize: 8,
-    borderRadius: '50%',
-    color: 'white',
-    backgroundColor: '#5b21b6',
-    paddingHorizontal: 6,
-    paddingVertical: 2,
-    marginRight: 4,
-    marginVertical: 2,
-  },
-  item: {
-    flexDirection: 'row',
-    marginBottom: 5,
-  },
-  bulletPoint: {
-    width: 10,
-    fontSize: 10,
-    color: '#5b21b6',
-  },
-  itemContent: {
-    flex: 1,
-    fontSize: 10,
-  },
-})
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import { PDFDownloadLink } from '@react-pdf/renderer'
 import { useEffect, useState } from 'react'
 import { MyDoc } from '@/components/pdfResume'
 // import MyDoc from '@/components/resume'
-
-import { BlobProvider, Document, Page, View, Text } from '@react-pdf/renderer'
+import { BlobProvider } from '@react-pdf/renderer'
 import PDFViewer from '@/components/PDFViewer'
 
 function Home() {
@@ -12,40 +10,40 @@ function Home() {
   useEffect(() => {
     setIsClient(true)
   }, [])
+
   return (
     <div className="flex h-screen">
       <div className="flex flex-row items-stretch flex-1 h-full">
         <div className="w-24 bg-green-200"></div>
         <div className="flex flex-col justify-center flex-1 mx-auto">
           {isClient && (
-            <>
-              <BlobProvider document={<MyDoc text={text} />}>
-                {({ blob, url, loading, error }) => {
-                  return <PDFViewer file={url} loading={loading}></PDFViewer>
-                }}
-              </BlobProvider>
+            <BlobProvider document={<MyDoc text={text} />}>
+              {({ blob, url, loading, error }) => {
+                return (
+                  <>
+                    <PDFViewer file={url} loading={loading}></PDFViewer>
+                    <div className="flex justify-between">
+                      <a
+                        href={url}
+                        download="resume.pdf"
+                        className="px-4 py-2 text-center text-white bg-green-500 rounded hover:bg-green-700"
+                      >
+                        {loading ? 'Loading document...' : 'Download File'}
+                      </a>
 
-              <div className="flex justify-between">
-                <PDFDownloadLink
-                  document={<MyDoc />}
-                  fileName="resume.pdf"
-                  className="px-4 py-2 text-center text-white bg-green-500 rounded hover:bg-green-700"
-                >
-                  {({ loading }) =>
-                    loading ? 'Loading document...' : 'Download File'
-                  }
-                </PDFDownloadLink>
-
-                <button
-                  className="px-4 py-2 text-center text-white bg-green-500 rounded hover:bg-green-700"
-                  onClick={() => {
-                    setText((actualState) => actualState + 'Aurora')
-                  }}
-                >
-                  Add Text
-                </button>
-              </div>
-            </>
+                      <button
+                        className="px-4 py-2 text-center text-white bg-green-500 rounded hover:bg-green-700"
+                        onClick={() => {
+                          setText((actualState) => actualState + 'Aurora')
+                        }}
+                      >
+                        Add Text
+                      </button>
+                    </div>
+                  </>
+                )
+              }}
+            </BlobProvider>
           )}
         </div>
       </div>
