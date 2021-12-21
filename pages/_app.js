38c6c9f93c82c186124972d7ea85e9725ff3bb4b
omnia-web <omnia.webmail@gmail.com>
2021-12-21 10:42:16 +0100

    header footer fix

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,11 @@ function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no ,height=device-height"
         />
       </Head>
-      <div className="flex flex-col min-h-screen ">
-        <div className="bg-indigo-500 h-[75px]">NAVIGATION</div>
-        <main className="relative flex flex-col self-center flex-1 w-full max-w-screen-xl overflow-hidden ">
+      <div className="flex flex-col justify-around min-h-screen">
+        <div className="bg-indigo-500 h-[75px] flex justify-center">
+          <div className="w-full max-w-screen-xl"> NAVIGATION</div>
+        </div>
+        <main className="relative flex flex-col self-center flex-1 w-full max-w-screen-xl overflow-hidden max-h-[calc(100vh-75px)]  min-h-[calc(100vh-75px)]">
           <Component {...pageProps} />
         </main>
         <div className="bg-indigo-500 ">FOOTER</div>
--- pages/index.js
+++ pages/index.js
@@ -29,12 +29,15 @@ function Home() {
   return (
     <>
       {isClient && (
-        <div className="flex flex-col flex-1 lg:flex-row lg:max-h-[calc(100vh-75px)] justify-center">
+        <div className="flex flex-col justify-center flex-1 lg:flex-row">
           <Backdrop show={showForm && !isDesktop} />
           <SettingsContainer
             show={showForm || isDesktop}
             className="fixed inset-0 top-0 z-10 p-4 m-4 overflow-auto rounded-md lg:w-1/2 lg:p-4 pb-28 lg:static lg:block bg-gradient-to-b from-purple-500 to-indigo-500 no-scrollbar"
           >
+            <Form />
+            <Form />
+            <Form />
             <Form />
           </SettingsContainer>
           <BlobProvider document={<MyDoc text={text} />}>
